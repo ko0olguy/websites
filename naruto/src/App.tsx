477cdffafdda188563f22a4b328cc24cf60d6989
ko0olguy <134905919+ko0olguy@users.noreply.github.com>
2023-10-01 15:06:56 -0400

    Fixed Bugs.

--- naruto/src/App.tsx
+++ naruto/src/App.tsx
@@ -1,8 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "./components/ui/card"
 import "./App.css"
-import { motion } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion"
+import { ThemeProvider } from "@/components/theme-provider"
+import { wrap } from "popmotion";
+import img from "./components/img.png"
+
+
+const jutsuVariants = {
+  initial: {
+    y: 1000
+  },
+  animate: {
+    y: 0
+  },
+  exit: {
+    y: 1000
+  }
+}
 
 const baseURL = 'https://dattebayo-api.onrender.com/';
 var randomNumber = Math.floor(Math.random() * 285) + 1;
@@ -27,92 +43,150 @@ const getRandomCharacters = async (limit = 5) => {
 
 const CharacterCard = ({ character }) => {
   const { name, images, jutsu } = character;
+  const [expandedJutsus, setExpandedJutsus] = useState(false);
+  const [expandedImages, setExpandedImages] = useState(false);
+
+  const limitedJutsus = Array.isArray(jutsu) ? jutsu.slice(0, 4) : [];
+  const displayJutsus = expandedJutsus ? jutsu : limitedJutsus;
+
   return (
-    <motion.div className='justify-center flex-wrap mx-auto min-w-max mt-8 character-card' initial={{ x: 10000 }} animate={{ x: 0 }}>
-      <Card className='justify-center flex-wrap min-h-max'>
-        <div className="border p-4 m-4 content-center">
-          <CardHeader>
-            <CardTitle className='text-lg font-semibold mb-5'>{name}</CardTitle>
-          </CardHeader>
-          <CardContent className='grid justify-center'>
-            <img src={images} alt={name} className="w-full h-64 object-cover mb-5" />
-            {Array.isArray(jutsu) && (
-              <CardDescription>
-                <h4 className="text-sm font-semibold mb-2 mt-2">Jutsus:</h4>
-                <ul className="my-0 py-2 list-none">
-                  {jutsu.map((juts, index) => (
-                    <li key={index}>{juts}</li>
-                  ))}
-                </ul>
-              </CardDescription>)}
+      <motion.div className='justify-center flex-wrap mx-auto min-w-max mt-8 character-card'>
+        <Card className='justify-center flex-wrap min-h-max'>
+          <div className="border p-4 m-4 content-center">
+            <CardHeader>
+              <CardTitle className='text-lg font-semibold mb-5'>{name}</CardTitle>
+            </CardHeader>
+            <CardContent className='grid justify-center'>
+              {Array.isArray(images) && images.map((image, index) => (
+                <img key={index} src={image} alt={`${name}-image-${index}`} className="w-full h-64 object-cover mb-5" />
+              ))}
+              {Array.isArray(displayJutsus) && jutsu && (
+                <CardDescription>
+                  <h4 className="text-sm font-semibold mb-2 mt-2">Jutsus:</h4>
+                  <ul className="my-0 py-2 list-none">
+                    {displayJutsus.map((juts, index) => (
+                      <motion.li variants={jutsuVariants} initial='initial' animate='animate' exit='exit' key={index}>{juts}</motion.li>
+                    ))}
+                  </ul>
+                  {jutsu.length > 4 ? (
+                    <button onClick={() => setExpandedJutsus(!expandedJutsus)}>
+                      {expandedJutsus ? 'Collapse Jutsus' : 'Expand Jutsus'}
+                    </button>
+                  ): null}
+                </CardDescription>
+              )}
             </CardContent>
             <CardFooter>
-              {/* Add any additional footer content if needed */}
+              {images.length > 1 && (
+                <button onClick={() => setExpandedImages(!expandedImages)}>
+                  {expandedImages ? 'Expand Images' : 'Collapse Images'}
+                </button>
+              )}
             </CardFooter>
-        </div>
-      </Card>
-    </motion.div>
-    
+          </div>
+        </Card>
+      </motion.div>
   );
 };
 
-const RandomCharacterList = () => {
-  const [characters, setCharacters] = useState([]);
-  const [currentCardIndex, setCurrentCardIndex] = useState(0);
 
-  useEffect(() => {
-    const fetchRandomCharacters = async () => {
-      try {
-        const data = await getRandomCharacters();
-        setCharacters(data.characters);
-      } catch (error) {
-        console.log('Error fetching random characters:', error);
+  const RandomCharacterList = () => {
+    const [characters, setCharacters] = useState([]);
+    const [[cardsIndex, direction], setCardsIndex] = useState([0, 0]);
+    const paginate = (newDirection: number) => {
+      setCardsIndex([cardsIndex + newDirection, newDirection]);
+    };
+    const cardIndex = wrap(0, characters.length, cardsIndex);
+
+    useEffect(() => {
+      const fetchRandomCharacters = async () => {
+        try {
+          const data = await getRandomCharacters();
+          setCharacters(data.characters);
+          setCardsIndex([0, 0]);
+        } catch (error) {
+          console.log('Error fetching random characters:', error);
+        }
+      };
+  
+      fetchRandomCharacters();
+    }, []);
+  
+    if (characters.length === 0 || !characters[cardIndex]) {
+      return null; 
+    }
+  
+    const variants = {
+      enter: (direction: number) => {
+        return {
+          x: direction > 0 ? 1000 : -1000,
+          opacity: 0
+        };
+      },
+      center: {
+        zIndex: 1,
+        x: 0,
+        opacity: 1
+      },
+      exit: (direction: number) => {
+        return {
+          zIndex: 0,
+          x: direction < 0 ? 1000 : -1000,
+          opacity: 0
+        };
       }
     };
-
-    fetchRandomCharacters();
-  }, []);
-
-  const handleNext = () => {
-    setCurrentCardIndex((prevIndex) => (prevIndex + 1) % characters.length);
+    const swipeConfidenceThreshold = 10000;
+    const swipePower = (offset: number, velocity: number) => {
+      return Math.abs(offset) * velocity;
   };
-
-  const handlePrev = () => {
-    setCurrentCardIndex(
-      (prevIndex) => (prevIndex - 1 + characters.length) % characters.length
+    return (
+      <AnimatePresence initial={false} custom={direction}>
+        <div className='justify-center w-100 m-auto'>
+          <motion.div
+            key={cardIndex}
+            className="flex overflow-x-auto"
+            custom={direction}
+            variants={variants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            transition={{
+              x: { type: "spring", stiffness: 300, damping: 30 },
+              opacity: { duration: 0.2 }
+            }}
+            drag="x"
+            dragConstraints={{ left: 0, right: 0 }}
+            dragElastic={0.5}
+            onDragEnd={(e, { offset, velocity }) => {
+              const swipe = swipePower(offset.x, velocity.x);
+              e.preventDefault();
+              if (swipe < -swipeConfidenceThreshold) {
+                paginate(1);
+              } else if (swipe > swipeConfidenceThreshold) {
+                paginate(-1);
+              }
+            }}
+          >
+            <CharacterCard
+              key={characters[cardIndex].id}
+              character={characters[cardIndex]}
+            />
+          </motion.div>
+        </div>
+      </AnimatePresence>
     );
   };
-
-  return (
-    <div className="flex overflow-x-auto relative">
-      {characters.length > 0 && (
-        <CharacterCard
-          key={characters[currentCardIndex].id}
-          character={characters[currentCardIndex]}
-        />
-      )}
-      <button
-        className="absolute top-1/2 transform -translate-y-1/2 left-2 bg-gray-500 p-2 rounded"
-        onClick={handlePrev}
-      >
-        &lt;
-      </button>
-      <button
-        className="absolute top-1/2 transform -translate-y-1/2 right-2 bg-gray-500 p-2 rounded"
-        onClick={handleNext}
-      >
-        &gt;
-      </button>
-    </div>
-  );
-};
+  
 
 export default function App() {
   return (
-    <div>
-      <h1>Naruto Characters</h1>
-      <RandomCharacterList />
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div>
+        <img src={img} className='justify-self-center w-64 mx-auto'></img>
+        <RandomCharacterList />
+      </div>
+    </ThemeProvider>
   );
 }
 
